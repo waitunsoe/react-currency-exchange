14c0f688c8e04183c30d41d8ae12179324e59595
Wai Tun Soe <waitunsoe.dev@gmail.com>
2023-09-16 02:44:59 +0630

    axios installatio & making useAxios custom hook

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const useAxios = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  return {data, error, loading};
+};
+
+export default useAxios;
